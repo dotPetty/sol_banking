1b3136093215296642dacbd5e75234531998715b
rocketRexTech <jordan.dotpetty@gmail.com>
2024-06-18 13:47:10 -0400

    Home screen welcome message and animated money counter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+import React from 'react';
 
 const Home = () => {
+
+  const loggedIn = { firstName: 'dot' }
+
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                    type= 'greeting'
+                    title= 'Welcome'
+                    user= {loggedIn?.firstName || 'Guest'}
+                    subtext= 'Access and manage your account and transactions efficiently.'
+                />
+
+                <TotalBalanceBox 
+                    accounts= {[]}
+                    totalBanks= {1}
+                    totalCurrentBalance= {10000}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
