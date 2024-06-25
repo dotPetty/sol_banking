7678d958bbb095a28b422b399af69b55327c0903
rocketRexTech <jordan.dotpetty@gmail.com>
2024-06-24 20:45:39 -0400

    my banks page

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,13 +2,14 @@ import { formatAmount } from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import Copy from './Copy';
 
 const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) => {
   return (
     <div className='flex flex-col'>
         <Link
-            href='/'
-            className='bank-card'
+            href={`/transaction-history/?id=${account.appwriteItemId}`}
+            className='bank-card min-w-[290px]'
         >
             <div className='bank-card_content'>
                 <div>
@@ -33,7 +34,7 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                     </div>
 
                     <p className='text-14 font-semibold tracking-[1.1px] text-white'>
-                    ●●●● ●●●● ●●●● <span className='text-16'>1234</span>
+                    ●●●● ●●●● ●●●● <span className='text-16'>{account?.mask}</span>
                     </p>
                 </article>
             </div>
@@ -63,6 +64,8 @@ const BankCard = ({ account, userName, showBalance = true }: CreditCardProps) =>
                 className='absolute top-0 left-0'
             />
         </Link>
+
+        {showBalance && <Copy title={account?.shareableId} />}
     </div>
   )
 }
